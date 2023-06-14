27ffc8f0b93136f0529b7ae2bfd1ac1319cda2f5
Gustavo souza <santos.gs708@gmail.com>
2023-06-14 07:45:45 -0300

    primeiro commit

---
+++ php_com_pdo.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Tempo de geração: 14/06/2023 às 12:33
+-- Versão do servidor: 10.4.22-MariaDB
+-- Versão do PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `php_com_pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_status`
+--
+
+CREATE TABLE `tb_status` (
+  `id` int(11) NOT NULL,
+  `status` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tb_status`
+--
+
+INSERT INTO `tb_status` (`id`, `status`) VALUES
+(1, 'pendente'),
+(2, 'realizado');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_tarefas`
+--
+
+CREATE TABLE `tb_tarefas` (
+  `id` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL DEFAULT 1,
+  `tarefa` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data_cadastrado` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tb_tarefas`
+--
+
+INSERT INTO `tb_tarefas` (`id`, `id_status`, `tarefa`, `data_cadastrado`) VALUES
+(4, 2, 'lavar', '2023-06-10 20:37:33'),
+(5, 2, 'correr', '2023-06-10 20:38:13'),
+(6, 2, 'correr', '2023-06-10 21:00:53'),
+(7, 1, 'lavar', '2023-06-10 21:01:17'),
+(8, 1, 'sair ', '2023-06-10 21:09:44');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Despejando dados para a tabela `tb_usuarios`
+--
+
+INSERT INTO `tb_usuarios` (`id`, `nome`, `email`, `senha`) VALUES
+(2, 'gustavo', 'ss@gmail.com', '12345678'),
+(3, 'gustvo', 's@gmail.com', '12345678'),
+(4, 'ustavo', 'sg@gmail.com', '12345678');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_status` (`id_status`);
+
+--
+-- Índices de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_status`
+--
+ALTER TABLE `tb_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `tb_tarefas`
+--
+ALTER TABLE `tb_tarefas`
+  ADD CONSTRAINT `tb_tarefas_ibfk_1` FOREIGN KEY (`id_status`) REFERENCES `tb_status` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
